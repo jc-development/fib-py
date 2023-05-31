307cf8eca83c5b437c91c90bf4aea8093dead7ce
jc-development <joe.cardella3@gmail.com>
2023-05-31 08:28:07 -0400

    adding setup to module

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="fib_py",
+    version="0.0.1",
+    author="Joe C",
+    author_email="joe.cardella3@gmail.com",
+    description="Calculates a Fibonacci number",
+    long_description="A basic library that calcs a Fibonacci number",
+    long_description_content_type="text/markdown",
+    url="https://githubcom/jc-development/fib-py",
+    install_requires=[],
+    packages=find_packages(exclude=("tests",)),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    tests_require=["pytest"],
+)
