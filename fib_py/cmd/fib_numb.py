ebd3d9b8fe962ec0de0652325545b9eb9eda7775
jc-development <joe.cardella3@gmail.com>
2023-06-02 08:27:40 -0400

    config

--- fib_py/cmd/fib_numb.py
+++ fib_py/cmd/fib_numb.py
@@ -1,17 +1,15 @@
 import argparse
 
-from fib_py.fib_calcs.fib_number import recurring_fibonacci_number
+from fib_py.fib_calcs.fib_number \
+    import recurring_fibonacci_number
 
 
 def fib_numb() -> None:
     parser = argparse.ArgumentParser(
-        description="Calculate the Fibonacci number of a given number."
-    )
-    parser.add_argument("--number", action='store', type=int, required=True,
-                        help="The number to calculate the Fibonacci number of.")
-
+        description='Calculate Fibonacci numbers')
+    parser.add_argument('--number', action='store',
+                        type=int, required=True,
+                        help="Fibonacci number to be calculated")
     args = parser.parse_args()
-    print(
-        f"The Fibonacci number of {args.number} is: "
-        f"{recurring_fibonacci_number(number=args.number)}."
-    )
+    print(f"Your Fibonacci number is: "
+          f"{recurring_fibonacci_number(number=args.number)}")
--- setup.py
+++ setup.py
@@ -1,29 +1,31 @@
 from setuptools import find_packages, setup
 
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
 
 setup(
     name="fib_py",
     version="0.0.1",
     author="Joe C",
-    author_email="joe.cardella3@gmail.com",
+    author_email="",
     description="Calculates a Fibonacci number",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://githubcom/jc-development/fib-py",
+    url="https://github.com/jc-development/fib-py",
     install_requires=[],
     packages=find_packages(exclude=("tests",)),
     classifiers=[
         "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
-    tests_require=["pytest"],
     entry_points={
         'console_scripts': [
             'fib-number = fib_py.cmd.fib_numb:fib_numb',
         ],
     },
+    python_requires='>=3',
+    tests_require=['pytest'],
 )
